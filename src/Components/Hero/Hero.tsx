e525b76e62287bf8f9709e0c5a975ee4f0045201
Taylor Kelley <taylorgkelley@outlook.com>
2024-06-11 16:09:32 -0500

    Adding photo and hero section styles

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -5,7 +5,11 @@ export default function Hero() {
   return (
     <div className="hero__container">
       <div className="image__container">
-        <img src="" alt="Image of Taylor Kelley" className="image" />
+        <img
+          src="/Images/pictureOfMe.jpg"
+          alt="Image of Taylor Kelley"
+          className="image"
+        />
       </div>
       <h1 className="title__name">Taylor Kelley</h1>
       <h3 className="title__profession">{'<React Developer />'}</h3>
