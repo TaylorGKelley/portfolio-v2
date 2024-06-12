6bd87d0367a566f36d3acf5d89c53af7b1c42615
Taylor Kelley <taylorgkelley@outlook.com>
2024-06-11 19:23:18 -0500

    Adding hero section styling and first commit from mac

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -3,17 +3,21 @@ import './Hero.css';
 
 export default function Hero() {
   return (
-    <div className="hero__container">
-      <div className="image__container">
-        <img
-          src="/Images/pictureOfMe.jpg"
-          alt="Image of Taylor Kelley"
-          className="image"
-        />
+    <div className="hero__container flex-column">
+      <div className="flex-column flex-center">
+        <div className="image__container">
+          <img
+            src="/Images/pictureOfMe.jpg"
+            alt="Image of Taylor Kelley"
+            className="image"
+          />
+        </div>
+        <h1 className="title__name">Taylor Kelley</h1>
+        <h3 className="title__profession">{'<React Developer />'}</h3>
       </div>
-      <h1 className="title__name">Taylor Kelley</h1>
-      <h3 className="title__profession">{'<React Developer />'}</h3>
-      <div className="hero__scroll-icon"></div>
+      <div>
+        <div className="hero__scroll-icon"></div>
+      </div>
     </div>
   );
 }
